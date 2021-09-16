6c99658bdc3ffdea19f7ac61b907fc549a3a3701
vladimir <vladimir.obradovic.sa@hotmail.com>
2021-09-16 15:45:12 +0200

    upload Repo04 rjesenja

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main (){
+    // only change code below this line
+     var myFirstName = "Vladimir";
+     var myLastName = "Obradovic";
+     var myHomeTown = "Sarajevo"
+    // Only change code above this line
+    return {
+       myFirstName,
+       myLastName,
+       myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
